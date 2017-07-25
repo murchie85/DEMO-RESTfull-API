7f54a164d0328926d6bb69456017d85f141eabe1
Adam <murchie85@gmail.com>
2017-07-25 21:11:32 +0100

    f

--- src/routes/customers.php
+++ src/routes/customers.php
@@ -4,6 +4,11 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 
 //********************INFO***********************
+//
+//  INFORMATION TO BE FOUND IN documents/blogg/Restfull...
+//
+//
+//
 //CALL WITH URL http://twistedpedia.co.nf/public/index.php/api/joke/58
 //
 //********************************************************************
@@ -67,4 +72,52 @@ $app->get('/api/joke/{joke_index}', function(Request $request, Response $respons
             //e for errors exception handling
              echo '{"error": {"text": '.$e->getMessage().'}';
         }
+});
+
+
+/*
+
+    USE REST EASY OR ANOTHER REST HARNESS
+    URI = http://twistedpedia.co.nf/public/index.php/api/joke/add
+    DONT FORGET TO SET METHOD TO = POST
+    HEADERS 
+             NAME = Content-type
+             Value= application/json
+
+    example body to pass in 
+    {
+    "joke" : "I am a joke",
+    "Joke_score" : "1"
+
+    }
+*/
+
+// Add a joke
+$app->post('/api/joke/add', function(Request $request, Response $response){
+    $joke = $request->getParam('joke');
+    $Joke_score = $request->getParam('Joke_score');
+
+
+
+    $sql = "INSERT INTO joke (joke,Joke_score) VALUES
+    (:joke,:Joke_score)";
+
+    try{
+        // Get DB Object
+        $db = new db();
+        // Connect
+        $db = $db->connect();
+
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindParam(':joke', $joke);
+        $stmt->bindParam(':Joke_score',  $Joke_score);
+
+        $stmt->execute();
+
+        echo '{"notice": {"text": "Joke Added"}';
+
+    } catch(PDOException $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+    }
 });
